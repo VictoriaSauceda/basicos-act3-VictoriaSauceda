e133c98d8f7123934f15d4acef00d1a8f4c5fa45
VictoriaSauceda <a01742388@itesm.mx>
2021-08-15 20:18:07 +0000

    Resolviendo ejercicios

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -1,6 +1,15 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    n1 = int(input('Dame un número: '))
+    n2 = int(input('Dame un número: '))
+
+    suma = n1 + n2
+    resta = n1 - n2
+    multiplicación = n1 * n2
+
+    print('Suma: '+str(suma))
+    print('Resta: '+str(resta))
+    print('Multiplicación: '+str(multiplicación))
 
 if __name__ == '__main__':
     main()
